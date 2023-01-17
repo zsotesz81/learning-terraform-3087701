c059d42055f0982420866fe34dbd6537b534bee5
Zsolt Nyerges <Zsolt_Nyerges@epam.com>
2023-01-17 14:27:47 +0100

    define security group module

--- main.tf
+++ main.tf
@@ -22,11 +22,25 @@ resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_group_ids = [aws_security_group.web.id]
+  vpc_security_group_ids = [modules.web-sg.security_group_id]
 
   tags = {
     Name = "HelloWorld"
   }
+}
+
+module "web-sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "4.17.1"
+  name= "web_new"
+
+  vpc_id = data.aws_vpc.default.id
+  
+  ingress_rules = ["http-80-tcp", "https-443-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+
+  egress_rules = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
 }
 
 resource "aws_security_group" "web" {
