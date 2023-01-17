329648ee4f3f92d3f20ec65ea88c80ca4e0570e9
Zsolt Nyerges <Zsolt_Nyerges@epam.com>
2023-01-17 15:48:57 +0100

    create dev vpc

--- main.tf
+++ main.tf
@@ -33,7 +33,7 @@ resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_group_ids = [module.web_sg.security_group_id]
+  vpc_security_group_ids = [module.web.security_group_id]
 
   tags = {
     Name = "HelloWorld"
