cadc2d755571fc6a6a60e3145fceae55bb821431
Zsolt Nyerges <Zsolt_Nyerges@epam.com>
2023-01-17 14:36:14 +0100

    removed old definition of security group

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@ resource "aws_instance" "web" {
 module "web_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.17.1"
-  name= "web_new"
+  name= "web"
 
   vpc_id = data.aws_vpc.default.id
   
@@ -41,41 +41,4 @@ module "web_sg" {
 
   egress_rules = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
-}
-
-resource "aws_security_group" "web" {
-  name        = "web"
-  description = "Allow http and https in. Allow everything out"
-
-  vpc_id = data.aws_vpc.default.id
-}
-
-resource "aws_security_group_rule" "web_http_in" {
-  type        = "ingress"
-  from_port   = 80
-  to_port     = 80
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.web.id
-}
-
-resource "aws_security_group_rule" "web_https_in" {
-  type        = "ingress"
-  from_port   = 443
-  to_port     = 443
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.web.id
-}
-
-resource "aws_security_group_rule" "web_everything_out" {
-  type        = "egress"
-  from_port   = 0
-  to_port     = 0
-  protocol    = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.web.id
 }
