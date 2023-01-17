b51b8385c7ac0885676fb1f8065450243d44beff
Zsolt Nyerges <Zsolt_Nyerges@epam.com>
2023-01-17 15:46:47 +0100

    create dev vpc

--- main.tf
+++ main.tf
@@ -45,7 +45,7 @@ module "web_sg" {
   version = "4.17.1"
   name= "web"
 
-  vpc_id = module.vpc.id
+  vpc_id = module.vpc.vpc_id
   
   ingress_rules = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
